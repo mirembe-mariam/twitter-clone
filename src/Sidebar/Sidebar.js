e32f7ea3f9b60aae8ae45bc3dd5009b3c23ec769
mirembe-mariam <mirembemariam3@gmail.com>
2023-06-03 16:52:34 +0300

    created componnet sidebar and installed material ui

---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Sidebar.css"
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <h1>sidebar</h1>
+        {/*Twitter icon */}
+        
+         {/*SidebarOption*/}
+         {/*SidebarOption*/}
+         {/*SidebarOption*/}
+         {/*SidebarOption*/}
+         {/*SidebarOption*/}
+         {/*SidebarOption*/}
+         {/*SidebarOption*/}
+         {/*SidebarOption*/}
+         {/*Button..Tweet*/}
+    </div>
+  )
+}
+
+export default Sidebar
